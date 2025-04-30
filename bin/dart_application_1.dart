f8ae91a1e9c5042cde95f02450d05f415ecc5c35
ameen <ameenzullaq@gmail.com>
2025-04-30 10:55:13 +0300

    training

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,4 +1,24 @@
-
 void main() {
-  print('Hello world');
+  SomeClass1 someClass1 = SomeClass1();
+  print(someClass1.myField);
+  someClass1.myMethod();
+  print('==========================================');
+  SomeClass2 someClass2 = SomeClass2();
+  print(someClass2.myField);
+  someClass2.myMethod();
+}
+
+class AnotherClass {
+  int myField = 42;
+  void myMethod() => print(myField);
+}
+
+class SomeClass1 extends AnotherClass {}
+
+class SomeClass2 implements AnotherClass {
+  @override
+  int myField = 50;
+
+  @override
+  void myMethod() {}
 }
